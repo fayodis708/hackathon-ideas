41b4a67da23c1020278cdb72e5d77aa741bc29e4
fayodis708 <fayodis708@gmail.com>
2025-04-11 11:34:22 +0000

    Add 28.ts

---
+++ src/28.ts
@@ -0,0 +1,11 @@
+// Example code 1
+function exampleFunction() {
+  console.log("This is an example function.");
+}
+
+// Example code 2
+let counter = 0;
+while (counter < 10) {
+  console.log(`Counter value: ${counter}`);
+  counter++;
+}
