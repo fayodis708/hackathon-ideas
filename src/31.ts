482b0b2af2c735ca852ef3db96e3a81e5efa2470
fayodis708 <fayodis708@gmail.com>
2025-04-25 12:10:23 +0000

    Add 31.ts

---
+++ src/31.ts
@@ -0,0 +1,5 @@
+// This is a simple example to demonstrate how to use TypeScript.
+// TypeScript can help catch potential errors before the program runs.
+
+let str: string = "Hello, TypeScript!";
+console.log(str);
